c38f51b99cd10ed9e0eb8efb79685ed20f07232f
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-01-08 10:19:34 +0100

    Add ListForm

---
+++ src/components/ListForm/ListForm.js
@@ -0,0 +1,34 @@
+import styles from './ListForm.module.scss';
+import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addList } from '../../redux/store';
+
+
+const ListForm = props => {
+
+    const dispatch = useDispatch();
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(addList({ listId: props.listId, title, description }));
+        setTitle('');
+        setDescription('');
+    };
+
+	return (
+        <form className={styles.listForm} onSubmit={handleSubmit}>
+            <label className={styles.label}>Title: </label>
+            <TextInput type="text" value={title} onChange={e => setTitle(e.target.value)} />
+            <label className={styles.label}>Description: </label>
+            <TextInput type="text" value={description} onChange={e => setDescription(e.target.value)} />
+            <Button>Add list</Button>
+        </form>
+	);
+};
+
+export default ListForm;
--- src/components/Lists/Lists.js
+++ src/components/Lists/Lists.js
@@ -1,4 +1,5 @@
 import styles from './Lists.module.scss';
+import ListForm from '../ListForm/ListForm';
 import { useSelector } from 'react-redux';
 import { getAllLists } from '../../redux/store';
 import { Link } from 'react-router-dom';
@@ -17,6 +18,7 @@ const Lists = () => {
               <p>{list.description}</p>
             </Link>
           ))}
+          <ListForm></ListForm>
         </section>
       );
   };
