39af4857ec4be115309ad79fa4562f1f29d5f809
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-01-16 12:24:48 +0100

    Remove spaces

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,12 +14,10 @@ const subreducers = {
 
 const reducer = combineReducers(subreducers);
 
-
 const store = createStore(
   reducer,
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-
 
 export default store;
